f0b4389b8e5e3ad7f8597c4e7e133f4fe3e8f495
Leandro Carvalho <leandrucs@gmail.com>
2024-10-07 11:48:37 -0300

    first commit

---
+++ src/app/components/custom-button.tsx
@@ -0,0 +1,7 @@
+export function CustomButton() {
+  return (
+    <button className="bg-blue-500 text-white px-6 py-2 rounded-xl hover:bg-blue-700 border border-black font-bold">
+      Botão
+    </button>
+  );
+}
