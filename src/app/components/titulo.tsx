835d348e0e4bb02cdd97a55267d0079a90756625
Leandro Carvalho <leandrucs@gmail.com>
2024-10-09 10:03:03 -0300

    style: personalizando o custom titulo

--- src/app/components/titulo.tsx
+++ src/app/components/titulo.tsx
@@ -1,3 +1,7 @@
 export function Titulo() {
-  return <h1 className="text-center text-4xl font-bold ">Título da página</h1>;
+  return (
+    <h1 className="text-center text-4xl font-bold underline">
+      Título da página
+    </h1>
+  );
 }
