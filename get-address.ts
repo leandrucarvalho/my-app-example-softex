097553a5ae9280d166ec0ea0ed15d8b8a0f5e4b3
Leandro Carvalho <leandrucs@gmail.com>
2024-10-16 11:51:27 -0300

    praticando try catch

--- get-address.ts
+++ get-address.ts
@@ -1,8 +1,13 @@
 async function getAddress(cep: string) {
   const url = `https://viacep.com.br/ws/${cep}/json/`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data.logradouro);
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.logradouro);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getAddress("53370140");
