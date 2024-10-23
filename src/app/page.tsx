7a1976c39c2ddf0ccd23c419c2a06766fe4b8b9e
Leandro Carvalho <leandrucs@gmail.com>
2024-10-23 11:52:21 -0300

    praticando o useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,41 @@
 "use client";
 
 import { getAddress } from "../../get-address";
-
-/* import { Cabecalho } from "./components/cabecalho";
-import { CustomButton } from "./components/custom-button";
-import { CustomImagem } from "./components/imagem";
-import { ListaItens } from "./components/lista-items";
-import { Paragrafo } from "./components/paragrafo";
-import { Quadrado } from "./components/quadrado";
-import { Rodape } from "./components/rodape";
-import { Titulo } from "./components/titulo"; */
-
-// type AvatarProps = {
-//   size?: number;
-// };
-//Props: pode ter valor padrão, pode ser opcional,
-
-// function Avatar({ size }: AvatarProps) {
-//   console.log(size);
-//   return (
-//     <img
-//       className="avatar"
-//       src="https://i.imgur.com/1bX5QH6.jpg"
-//       alt="Lin Lanying"
-//       width={size}
-//       height={size}
-//     />
-//   );
-// }
-
-// type CardProps = {
-//   children: React.ReactNode;
-// };
-
-// function Card({ children }: CardProps) {
-//   console.log(children);
-//   return (
-//     <div className="p-3 bg-blue-500 text-white border border-black rounded-lg">
-//       {children}
-//     </div>
-//   );
-// }
+import { useState } from "react";
 
 export default function Home() {
+  const [address, setAddress] = useState();
+  const [loading, setLoading] = useState(false);
+
   async function handleGetAddress() {
-    const result = await getAddress("53370140");
-    console.log(result);
+    setLoading(true);
+
+    try {
+      const result = await getAddress("53370140");
+      setAddress(result.logradouro);
+
+      console.log(result.logradouro);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
-    <div className="p-10 flex flex-col items-start gap-4">
-      <button
-        onClick={handleGetAddress}
-        className="px-3 py-2 rounded-lg bg-primary text-white font-bold"
-      >
-        Obter endereço
-      </button>
-      <p>
-        Endereço obtido: <span></span>
-      </p>
-    </div>
+    <>
+      <h1 className="p-2">Página Home</h1>
+      <div className="p-10 flex flex-col items-start gap-4">
+        <button
+          onClick={handleGetAddress}
+          className="px-3 py-2 rounded-lg bg-primary text-white font-bold"
+        >
+          Obter endereço
+        </button>
+        <p>
+          Endereço obtido: <span>{loading ? "Carregando..." : address}</span>
+        </p>
+      </div>
+    </>
   );
 }
